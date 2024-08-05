890076b17d1c34332442513ea68a0dce99dc618c
EnhancedTarget <levente.kalman922@gmail.com>
2024-08-05 23:32:07 +0100

    Create settings menu, add elements, create script. Need to finish script for volume slider, res dropdown, fullscreen toggle.

---
+++ Assets/Scripts/ManagerScripts/SettingsMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    //function for setting the volume
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
