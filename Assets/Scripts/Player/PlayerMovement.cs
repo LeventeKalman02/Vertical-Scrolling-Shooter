7b61942127e69d507fd8c9207e73156ecb3b2d11
EnhancedTarget <levente.kalman922@gmail.com>
2024-08-14 19:27:07 +0100

    Player Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 10f;
+    Rigidbody2D rb;
+    private Vector2 movement;
 
+    //awake to get the rigidbody component
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
+    //fixed update for running physics calculations
+    private void FixedUpdate()
+    {
+        Movement();
+    }
 
+    //move the player 
+    private void Movement()
+    {
+        //get the input for the X and Y axis
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //add the movement to the rb velocity
+        rb.velocity = movement * moveSpeed;
+    }
 }
