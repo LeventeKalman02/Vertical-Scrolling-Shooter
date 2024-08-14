365d57ada7f143189ae4d09cca270fd7e9ff7b53
EnhancedTarget <levente.kalman922@gmail.com>
2024-08-14 18:56:22 +0100

    Created pause menu, added resume, menu, quit buttons. All functional

---
+++ Assets/Scripts/ManagerScripts/PauseMenu.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    //variable to be able to check if the game is currently paused
+    //can be used in other scripts to check if the game is paused using PauseMenu.isPaused
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the Escape key is pressed then pause the game
+        //else resume
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+
+    //unfreeze time
+    //hide pause menu
+    //set variable to false
+    public void Resume()
+    {
+        //hide the display
+        pauseMenuUI.SetActive(false);
+        //unpause the game
+        Time.timeScale = 1f;
+        //set variable to false
+        isPaused = false;
+    }
+
+    //Freeze Time in the game
+    //display pause menu
+    //set variable to true
+    void Pause()
+    {
+        //show the display
+        pauseMenuUI.SetActive(true);
+        //pause the game
+        Time.timeScale = 0f;
+        //set variable to true
+        isPaused = true;
+    }
+
+    //Load the main menu scene
+    public void LoadMenu()
+    {
+        //Debug.Log("Loading Menu");
+        SceneManager.LoadScene(0);
+        //set the game time back to 1
+        Time.timeScale = 1f;
+        //set the variable back to false
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
