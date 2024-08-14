cac40d924a374eab13d0cceaed48629bece0eee2
EnhancedTarget <levente.kalman922@gmail.com>
2024-08-14 19:49:53 +0100

    added main menu audio

--- Assets/Scripts/ManagerScripts/MainMenuScript.cs
+++ Assets/Scripts/ManagerScripts/MainMenuScript.cs
@@ -7,6 +7,7 @@ public class MainMenuScript : MonoBehaviour
     //Quit the application if user presses the button
     public void ExitGame()
     {
+        Debug.Log("Exiting Game");
         Application.Quit();
     }
 }
