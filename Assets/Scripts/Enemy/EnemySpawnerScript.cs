dc64ad06be2a0e49dbbaa5c92a025c32eb15a123
EnhancedTarget <levente.kalman922@gmail.com>
2024-08-28 22:49:39 +0100

    spawnedenemies list

--- Assets/Scripts/Enemy/EnemySpawnerScript.cs
+++ Assets/Scripts/Enemy/EnemySpawnerScript.cs
@@ -7,7 +7,7 @@ public class EnemySpawnerScript : MonoBehaviour
 {
     [SerializeField] private float spawnRate = 1f;
     [SerializeField] private bool canSpawn = true;
-    [SerializeField] private int spawnedEnemies;
+    [SerializeField] public List<GameObject> spawnedEnemies = new List<GameObject>();
     [SerializeField] private int currLevel = 1;
 
     [SerializeField] private GameObject enemyPrefab;
@@ -26,15 +26,15 @@ public class EnemySpawnerScript : MonoBehaviour
         while (canSpawn)
         {
             //check to set the amount of enemies that can spawn in a level, This can be changed in the editor for each level.
-            if (currLevel == 1 && spawnedEnemies <= 9)
+            if (currLevel == 1 && spawnedEnemies.Count <= 9)
             {
                 SpawnEnemy();
             }
-            else if (currLevel == 2 && spawnedEnemies <= 14)
+            else if (currLevel == 2 && spawnedEnemies.Count <= 14)
             {
                 SpawnEnemy();
             }
-            else if (currLevel == 3 && spawnedEnemies <= 19)
+            else if (currLevel == 3 && spawnedEnemies.Count <= 19)
             {
                 SpawnEnemy();
             }
@@ -51,8 +51,8 @@ public class EnemySpawnerScript : MonoBehaviour
     //Instantiate the enemy prefab so that the enemies spawn
     private void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-        spawnedEnemies++;
+        GameObject obj = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        spawnedEnemies.Add(obj);
     }
 
     //if there are no more enemies to spawn in the level, display the level over screen
@@ -60,5 +60,20 @@ public class EnemySpawnerScript : MonoBehaviour
     {
         Time.timeScale = 0f;
         levelOverUI.SetActive(true);
+    }
+
+    //checks if there are any enemies alive
+    public void CheckAlive()
+    {
+        //loop over list of spawned enemies to check if there is any alive before starting the next wave
+        //reversed for loop
+        for (int i = spawnedEnemies.Count - 1; i >= 0; i--)
+        {
+            //check if entry is null, if true, remove from the list
+            if (spawnedEnemies[i] == null)
+            {
+                spawnedEnemies.RemoveAt(i);
+            }
+        }
     }
 }
